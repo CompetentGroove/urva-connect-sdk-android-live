fd80c0d5f7fb273a5d5797d35c652e0b173f3b07
Harpreet Singh <harpreet@competentgroove.com>
2022-05-27 14:50:15 +0530

    Inital commit

---
+++ AndroidLibraryForGitHubPackagesDemo-master/app/src/main/java/com/enefce/samples/androidlibdemo/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.enefce.samples.androidlibdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import competent.groove.urvaconnect.sdk.UrvaChat
+//import competent.groove.urvaconnect.sdk.UrvaChat
+//import com.enefce.libraries.sampleandroidlib.SampleAndroidLibMain
+import kotlinx.android.synthetic.main.activity_main.*
+
+//import github.tobsef.demo.HelloMaven;
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        var ass = UrvaChat.getInstance(this);
+
+
+
+        //Initialise the main class from the Sample Android Library module
+//        val sampleAndroidLibMain = SampleAndroidLibMain()
+//        tv_welcome.text = sampleAndroidLibMain.welcomeString
+    }
+}
