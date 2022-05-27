bff386d11587c47fd8a38821699b52cd4ea77d91
Harpreet Singh <harpreet@competentgroove.com>
2022-05-27 16:52:18 +0530

    Dependency add Set version 1.0.5

---
+++ UrvaConnectSdk/app/src/main/java/competent/groove/samples/project/MainActivity.kt
@@ -0,0 +1,30 @@
+package competent.groove.samples.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.enefce.samples.androidlibdemo.R
+import competent.groove.urvaconnect.sdk.UrvaChat
+
+//import competent.groove.urvaconnect.sdk.UrvaChat
+//import competent.groove.urvaconnect.sdk.UrvaChat
+//import com.enefce.libraries.sampleandroidlib.SampleAndroidLibMain
+
+//import github.tobsef.demo.HelloMaven;
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        var ass = UrvaChat
+
+
+
+        //Initialise the main class from the Sample Android Library module
+//        val sampleAndroidLibMain = SampleAndroidLibMain()
+//        tv_welcome.text = sampleAndroidLibMain.welcomeString
+    }
+}
