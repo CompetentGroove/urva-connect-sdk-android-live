e57e1e624aa33def151da8162a4b271443afa4ec
harpreetsingh <harpreet@competentgroove.com>
2022-11-28 18:17:43 +0530

    working on changing project of native libreary for testing

--- UrvaConnectSdk/app/src/main/java/competent/groove/samples/project/MainActivity.kt
+++ UrvaConnectSdk/app/src/main/java/competent/groove/samples/project/MainActivity.kt
@@ -2,19 +2,14 @@ package competent.groove.samples.project
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+//import competent.groove.urvaconnect.sdk.UrvaChat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        EditText email = findViewById(R.id.edit_text);
-//        Button validate = findViewById(R.id.button);
-//        validate.setOnClickListener(v -> {
-//            if (EmailValidate.getInstance().isValid(String.valueOf(email.getText())))
-//                Toast.makeText(MainActivity.this, getString(R.string.valid), Toast.LENGTH_SHORT).show();
-//            else
-//                Toast.makeText(MainActivity.this, getString(R.string.invalid), Toast.LENGTH_SHORT).show();
-//        });
+//        UrvaChat
+
     }
 }
